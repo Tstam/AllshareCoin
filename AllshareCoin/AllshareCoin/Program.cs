78adc512cdcebc17ed36c3bfc9b2a2a540209a9b
Timo Stam <ikbentimo@hotmail.com>
2018-05-11 16:01:21 +0200

    Solution setup

---
+++ AllshareCoin/AllshareCoin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllshareCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
